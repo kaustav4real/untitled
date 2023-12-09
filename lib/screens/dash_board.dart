65a99f20576e6f72ab91ea987be4a40035c70730
kaustav4real <kaustavkakoty4@gmail.com>
2023-12-10 00:09:56 +0530

    Contains login, and dashboard

--- lib/screens/dash_board.dart
+++ lib/screens/dash_board.dart
@@ -1,36 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:untitled/components/dashboard_screen/classes_assigned_posts.dart';
 import 'package:untitled/components/dashboard_screen/dashboard_app_bar.dart';
 import 'package:untitled/components/dashboard_screen/no_classes_assigned_card.dart';
-import 'package:untitled/state_management/user_auth.dart';
+import 'package:untitled/components/global/padded_text.dart';
+import 'package:untitled/models/assigned_class_models.dart';
 
 class DashBoard extends StatelessWidget {
   const DashBoard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final AuthController authController = Get.find<AuthController>();
+    List<ClassSchedule> classAssignedList = [
+      ClassSchedule(
+        subjectName: 'Formal Language And Theory Of Automation  ',
+        departmentName: 'Computer Science & Engineering',
+        semester: '5th',
+      ),
+      ClassSchedule(
+        subjectName: 'Distributed Systems',
+        departmentName: 'Computer Science & Engineering',
+        semester: '7th',
+      ),
+      ClassSchedule(
+        subjectName: 'Data Structures and Algorithms',
+        departmentName: 'Computer Science & Engineering',
+        semester: '3rd',
+      ),
+    ];
+
     return SafeArea(
       child: Scaffold(
         appBar: PreferredSize(
-          preferredSize: Size(MediaQuery.of(context).size.width, 80),
+          preferredSize: Size(MediaQuery.of(context).size.width, 75),
           child: const DashBoardAppBar(),
         ),
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         body: SingleChildScrollView(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const NoClassesAssigned(),
-
-              TextButton(
-                onPressed: () {
-                  authController.logout();
-                },
-                child: Text(
-                  'Logout',
-                  style: Theme.of(context).textTheme.titleSmall,
-                ),
-              )
+              const SizedBox(height: 30),
+              const PaddedText(text: 'YOUR CLASSES'),
+              const SizedBox(height: 30),
+              classAssignedList.isEmpty
+                  ? const NoClassesAssigned()
+                  : ClassesAssignedPost(schedule: classAssignedList)
             ],
           ),
         ),
--- lib/theme.dart
+++ lib/theme.dart
@@ -6,6 +6,19 @@ final ThemeData lightTheme=ThemeData(
   brightness: Brightness.light,
   scaffoldBackgroundColor: const Color(0XFFd7e3f7),
   textTheme: TextTheme(
+    bodyMedium: GoogleFonts.notoSans(
+        color: Colors.black54,
+        fontSize: 20,
+        fontWeight: FontWeight.w500
+    ),
+    labelSmall: GoogleFonts.notoSans(
+      color: Colors.black54,
+      fontSize: 13,
+    ),
+    labelMedium: GoogleFonts.notoSans(
+      color: Colors.black,
+      fontSize: 16,
+    ),
     titleSmall: GoogleFonts.montserrat(
       fontSize: 16,
       fontWeight: FontWeight.w400,
@@ -13,17 +26,14 @@ final ThemeData lightTheme=ThemeData(
     ),
     titleMedium: GoogleFonts.montserrat(
       fontSize: 20,
-      fontWeight: FontWeight.w600
+      fontWeight: FontWeight.w600,
+      color: Colors.black
     ),
     titleLarge: GoogleFonts.openSans(
       fontSize: 24,
       fontWeight: FontWeight.w700
     ),
-    bodyMedium: GoogleFonts.notoSans(
-      color: Colors.black54,
-      fontSize: 20,
-      fontWeight: FontWeight.w500
-    )
+
   ),
 );
 
